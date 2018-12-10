4de0cc18b100634137bea3bff1269b8af810521c
Fin Mavis <fin.mavis@yahoo.com>
2018-12-10 17:49:19 +0700

    Updates

---
+++ precache-manifest.d64d151f222986fa8e181ab53e5ee74e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad5744af9aae3e937906",
+    "url": "/exchange-rates/static/js/runtime~main.ad5744af.js"
+  },
+  {
+    "revision": "ba4638d06c87ee4f43c8",
+    "url": "/exchange-rates/static/js/main.ba4638d0.chunk.js"
+  },
+  {
+    "revision": "09ffbe1cb11dd8d751d8",
+    "url": "/exchange-rates/static/js/1.09ffbe1c.chunk.js"
+  },
+  {
+    "revision": "ba4638d06c87ee4f43c8",
+    "url": "/exchange-rates/static/css/main.6942239f.chunk.css"
+  },
+  {
+    "revision": "ca1a4fb4b20a69e4fa1132b6a2df3f94",
+    "url": "/exchange-rates/index.html"
+  }
+];
